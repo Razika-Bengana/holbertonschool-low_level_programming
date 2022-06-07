fa076b5d79e8e126defeb6d45083974fd261ad74
Razika-Bengana <5003@holbertonschool.com>
2022-06-07 11:55:32 +0200

    script 5 C

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"with proper grammar, but the outcome is a piece of art");
+printf("\"with proper grammar, but the outcome is a piece of art");
 return (0);
+}
