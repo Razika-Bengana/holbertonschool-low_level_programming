3b13a0a030cc5e49f5f32b9638528f8a8d012936
Razika-Bengana <5003@holbertonschool.com>
2022-06-08 09:59:43 +0200

    script 5 C

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+
+printf("\n");
+
+for (n = 1; n < 10; n++)
+{
+printf("%d", n);
+}
+
+getch();
+
+return (0);
+}
