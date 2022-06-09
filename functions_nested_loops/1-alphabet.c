b50dee3725e4b9fb95dafa6b68056e2606d95acd
Razika-Bengana <5003@holbertonschool.com>
2022-06-09 14:21:01 +0200

    script 1 nested loops

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - Prints alphabets 'a-z'
+ */
+
+	void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+
+	_putchar('\n');
+}
