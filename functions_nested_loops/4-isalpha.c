38c535529d7dbf3e05568efb073102553261d4c4
Razika-Bengana <5003@holbertonschool.com>
2022-06-10 11:55:17 +0200

    script 4 nested loops

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - checks if a character is in lowercase
+ * @c: An integer input
+ * Return: 1 if character is lowercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
