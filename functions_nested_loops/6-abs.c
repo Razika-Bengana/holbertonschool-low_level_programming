1c9086701c6f38899e7a6392bebb480d5057c15a
Razika-Bengana <5003@holbertonschool.com>
2022-06-10 15:00:35 +0200

    script 6 nested loops

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,11 +2,12 @@
 #include "main.h"
 
 /**
- * _abs - compute the absolute value of an integer
- *@n : The number to be checked
+ *_abs - compute the absolute value of an integer
+ *@n: The number to be checked
+ *Return: the absolute value of an integer
  */
 
-int _abs (int n)
+int _abs(int n)
 {
 	if (n >= 0)
 	return (n);
