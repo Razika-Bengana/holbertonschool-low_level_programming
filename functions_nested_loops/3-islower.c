c09870bb5fdaec00184d0950061656a05c6e782a
Razika-Bengana <5003@holbertonschool.com>
2022-06-10 10:01:23 +0200

    script 3 nested loops

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - checks if a character is in lowercase
+ * Return: 1 if character is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
