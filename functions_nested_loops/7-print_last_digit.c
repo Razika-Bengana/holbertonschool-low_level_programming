eb0b3e1280ac0b813c8d67ff894f1e023886f5f1
Razika-Bengana <5003@holbertonschool.com>
2022-06-11 13:45:08 +0200

    script 7 nested loops

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *print_last_digit - print the last digit of a number
+ *@n: The number to be checked
+ *Return: the last digit of a number
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+		n *= -1;
+	_putchar(n % 10 + '0');
+	return (n % 10);
+}
