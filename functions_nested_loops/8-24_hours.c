68163b6fe06120213680f6130cfe61b86a3e15e8
Razika-Bengana <5003@holbertonschool.com>
2022-06-12 10:55:47 +0200

    script 8 nested loops

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,12 +1,15 @@
 #include <stdlib.h>
 #include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of jack bauer
+ */
 void jack_bauer(void)
 {
 	int hour, minute;
 
 	for (hour = 0; hour < 24; hour++)
 	{
-		for (minute =0; minute < 60; minute++)
+		for (minute = 0; minute < 60; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
