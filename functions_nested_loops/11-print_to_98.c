dce6e4b461bb9d7a4d8f085696e1da66a576ef27
Razika-Bengana <5003@holbertonschool.com>
2022-06-12 23:30:04 +0200

    script 11 nested loops

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_to_98 - function that prints all numbers from n to 98
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+		for (; n <= 97; n++)
+			printf("%d, ", n);
+	else
+		for (; n > 98; n--)
+			printf("%d, ", n);
+	printf("98\n");
+}
