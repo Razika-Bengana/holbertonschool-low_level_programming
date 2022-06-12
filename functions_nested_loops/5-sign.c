6cc9b7db7868bbe51062358cfb151879dff5336f
Razika-Bengana <5003@holbertonschool.com>
2022-06-12 18:57:48 +0200

    script 5 nested loops

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*int print_sign(int n) - prints the sign of a number
+*print_sign - prints the sign of a number
 * @n: The number of which the sign will be printed.
 * Return: 1 if the number is greater than zero,
 	* 0 if the number is zero,
