8b61c92dbe2f02df6c349af8ca76a87aeba77c53
Razika-Bengana <5003@holbertonschool.com>
2022-06-12 19:09:21 +0200

    script 10 nested loops

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,7 +2,8 @@
 #include "main.h"
 /**
  * add - function that adds two integers and returns the result
- *@m: an integer; @n: an integer
+ *@m: an integer
+ *@n: an integer
  *Return: a + b result
  */
 
