295ee2c7878ba993889d4fbb06268ff6b6be65ef
Razika-Bengana <5003@holbertonschool.com>
2022-06-12 18:53:13 +0200

    script 1 nested loops

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *void print_alphabet(void) - print alphabets in lowercase
+ *print_alphabet - print alphabets in lowercase
  */
 
 	void print_alphabet(void)
