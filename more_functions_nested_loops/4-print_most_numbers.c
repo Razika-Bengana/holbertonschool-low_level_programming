b76c8123fbc676bf0e543d708b9771362516a36f
Razika-Bengana <5003@holbertonschool.com>
2022-06-13 20:32:53 +0200

    script 4 more nested loops

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_numbers - prints the numbers from 0 to 9, except 2 and 4
+* Return: always 0
+*/
+void print_most_numbers(void)
+{
+	int number;
+
+	number = 0;
+	while (number < 10)
+	{
+		if (number != 2 && number != 4)
+		{
+			_putchar (number + '0');
+		}
+	number++;
+}
+_putchar ('\n');
+}
