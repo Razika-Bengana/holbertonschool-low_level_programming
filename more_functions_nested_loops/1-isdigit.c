76756e368c1b21d1d3bd534d274664bb970368ca
Razika-Bengana <5003@holbertonschool.com>
2022-06-13 14:24:45 +0200

    script 1 more nested loops

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit - 0 through 9
+ * @c: define the variable to be tested
+ * Return: 1 if it is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
