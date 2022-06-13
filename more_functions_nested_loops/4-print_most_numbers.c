28cc53d755fc62baedbacfffbf32f799cbf17ee7
Razika-Bengana <5003@holbertonschool.com>
2022-06-13 20:33:56 +0200

    script 4 more nested loops

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_numbers - prints the numbers from 0 to 9, except 2 and 4
+* print_most_numbers - prints the numbers from 0 to 9, except 2 and 4
 * Return: always 0
 */
 void print_most_numbers(void)
