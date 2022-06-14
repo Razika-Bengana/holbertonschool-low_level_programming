97c30f8470463d4b7e924eb3d4ed1e85ec99b858
Razika-Bengana <5003@holbertonschool.com>
2022-06-14 15:33:29 +0200

    script 10 more nested loops

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle on the terminal
+ *@size: prints the size of the triangle
+ * Return: the result of void
+ */
+void print_triangle(int size)
+{
+
+	int i, j, k;
+
+	for (i=1; i<=size; i++)
+	{
+		for(k = size-i; k >= 1; k--)
+		{
+			_putchar(' ');
+		}
+		for (j=1; j<=i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar(10);
+	}
+}
