8f4233fdbd4a37f6f641dbed847ccb8646ec9979
Razika-Bengana <5003@holbertonschool.com>
2022-06-14 15:45:41 +0200

    script 10 more nested loops

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,16 +9,21 @@ void print_triangle(int size)
 
 	int i, j, k;
 
-	for (i=1; i<=size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for(k = size-i; k >= 1; k--)
+		for (k = size - i; k >= 1; k--)
 		{
 			_putchar(' ');
 		}
-		for (j=1; j<=i; j++)
+		for (j = 1; j <= i; j++)
 		{
 			_putchar('#');
 		}
 		_putchar(10);
 	}
+	if
+		(size <= 0)
+		{
+			_putchar (10);
+		}
 }
