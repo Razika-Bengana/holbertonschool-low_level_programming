e8c084ee7078ff72e6302a5c6452df7884129834
Razika-Bengana <5003@holbertonschool.com>
2022-06-15 14:20:18 +0200

    script 4 pointers

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
  * print_rev - prints a string in reverse
- *@s: a pointer
+ * @s: a pointer
  * Return: void
  */
 
 void print_rev(char *s)
 {
+
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++);
+	for (i = 0; s[i] != '\0'; i++)
+		;
 	for (i = i; i--;)
-	{
-		_putchar(s[i]);
-	}
-	_putchar (10);
+
+	_putchar(s[i]);
+	_putchar(10);
 }
