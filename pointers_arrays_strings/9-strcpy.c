b0f96dff8bcbfded11917e8de6cd16e39d23e3d1
Razika-Bengana <5003@holbertonschool.com>
2022-06-16 10:50:08 +0200

    script 9 pointers

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _strcpy - function that copies the string pointed to by src
- * @src : a pointer to a char
- * @dest : a pointer to a char
- * Return : dest
+ * @src: a pointer to a char
+ * @dest: a pointer to a char
+ * Return: the pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
