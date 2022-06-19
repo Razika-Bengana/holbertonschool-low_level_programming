f3db2d68e41ab822d630232ff083e475c30dc884
Razika-Bengana <5003@holbertonschool.com>
2022-06-19 23:21:47 +0200

    script 7 more pointers

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,4 +5,21 @@
  * Return: void
  */
 void puts_half(char *str)
-	
+{
+	int j, n, len;
+
+	len = 0;
+
+	for (j = 0; str[j] != '\0'; j++)
+		len++;
+
+	n = (len / 2);
+
+	if ((len % 2) == 1)
+		n = ((len + 1) / 2);
+
+	for (j = n; str[j] != '\0'; j++)
+		_putchar(str[j]);
+
+	_putchar('\n');
+}
