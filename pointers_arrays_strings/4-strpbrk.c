851489854416744f0f2626d510b9e1882e5778ca
Razika-Bengana <5003@holbertonschool.com>
2022-06-19 21:58:10 +0200

    script 4 more pointers 2

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include "main.h"
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes
+ * @s: contain number of bytes returned
+ * @accept: bytes to include from @s
+ * Return: number of bytes that are coming from @accept, or NULL for nothing
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	while (*s)
+	{
+
+	for (a = 0; accept[a]; a++)
+	{
+		if
+			(*s == accept[a])
+			return (s);
+	}
+		s++;
+	}
+	return (NULL);
+}
