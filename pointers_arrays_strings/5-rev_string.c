b1b02f159ffc8b922a869efa52bf2bbfd42cddb1
Razika-Bengana <5003@holbertonschool.com>
2022-06-19 23:34:10 +0200

    script 5 more pointers

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,4 +6,18 @@
  */
 void rev_string(char *s)
 {
-       
+	int a = 0;
+	int b;
+	char r = s[0];
+
+	while (s[a] != '\0')
+		a++;
+
+	for (b = 0; b < a; b++)
+	{
+		a--;
+		r = s[b];
+		s[b] = s[a];
+		s[a] = r;
+	}
+}
