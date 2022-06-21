0fe0641caa9923b70a30a9088b1a17c05b531363
Razika-Bengana <5003@holbertonschool.com>
2022-06-21 10:35:39 +0200

    script 4 recursion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * pow_recursion - returns the the value of x raised to the power of y
+ * @x: integer one
+ * @y: integer two
+ * Return: the factorial of a given number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
