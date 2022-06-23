c8dfe19364f63cfe7fd2857aec07600ac03d028b
Razika-Bengana <5003@holbertonschool.com>
2022-06-23 15:39:14 +0200

    script 7 pointers

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -5,4 +5,19 @@
  * @s: string that has to be encoded
  * Return: string (success)
  */
-char *leet(char *);
+char *leet(char *s)
+{
+	int a = 0;
+	int b;
+	char c[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char d[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	while (s[a])
+	{
+		for (b = 0; b < 10; b++)
+			if (s[a] == c[b])
+				s[a] = d[b];
+		a++;
+	}
+	return (s);
+}
