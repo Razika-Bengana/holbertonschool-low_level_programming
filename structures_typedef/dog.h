0e230c88a23a64f7e8feb1458687b19b6b6403fd
Razika-Bengana <5003@holbertonschool.com>
2022-06-29 22:25:49 +0200

    structures, typedef

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - define a new type struct dog
+ *@name : name (type = string)
+ *@age : age (type = float)
+ *@owner : owner (type = string)
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
