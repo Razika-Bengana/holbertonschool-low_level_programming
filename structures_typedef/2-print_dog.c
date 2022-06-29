574c2bbcffb63bccd90afdf2705480785538b130
Razika-Bengana <5003@holbertonschool.com>
2022-06-29 22:47:12 +0200

    script 2 structures, typedef

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,8 +15,8 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
+
+	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 }
