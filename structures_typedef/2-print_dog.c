e1b7fc1b784f37e5a5c441e0ee8cb2ef338571d1
Razika-Bengana <5003@holbertonschool.com>
2022-06-29 22:43:32 +0200

    script 2 structures, typedef

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ *print_dog - a function that prints a struct dog
+ *@d : a pointer
+ *Return : the printed result
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
