b225145d4d18d0423ed696b066faea887adbee2c
Razika-Bengana <5003@holbertonschool.com>
2022-06-30 15:24:05 +0200

    script 5 recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * sqrt2 - function that makes possible frm 1 to n
+ * @a : an integer
+ * @b : an integer
+ * return : 0 (success)
+ */
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the integer
+ * Return: the natural square root of a number
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
