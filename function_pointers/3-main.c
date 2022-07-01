f2a83840720a53d3c24056a97c9596d136b19c91
Razika-Bengana <5003@holbertonschool.com>
2022-07-01 23:57:14 +0200

    main file

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	b = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 
-	if (f == NULL)
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
