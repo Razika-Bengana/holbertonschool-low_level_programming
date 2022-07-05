4f6a6a083a034c63a138360ea008e76bdb0e9e99
Razika-Bengana <5003@holbertonschool.com>
2022-07-05 22:39:42 +0200

    script 3 variadic functions

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_all - function that prints anything (char, integer, float, string)
+ * @format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	int a = 0;
+	char *s;
+	va_list list;
+
+	va_start(list, format);
+
+	while (format && format[a])
+	{
+
+		switch (format[a++])
+		{
+
+		case 'c':
+			printf("%c", va_arg(list, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(list, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(list, double));
+			break;
+		case 's':
+			s = va_arg(list, char *);
+			if (s == NULL)
+				printf("nil");
+			else
+			printf("%s", s);
+			break;
+
+		default:
+			continue;
+		}
+		if (format[a])
+			printf(", ");
+	}
+	putchar(10);
+}
