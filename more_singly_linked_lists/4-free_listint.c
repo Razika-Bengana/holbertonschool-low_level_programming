792b7cbc3db51bf0cb8c769df13a541888deac0f
Razika-Bengana <5003@holbertonschool.com>
2022-07-17 18:38:15 +0200

    more singly linked lists

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * free_listint - function that frees a listint_t list
- * @head: pointer to the head of the linked list
+ * @head: a pointer to the head of the linked list
  * Return: void
  */
 
