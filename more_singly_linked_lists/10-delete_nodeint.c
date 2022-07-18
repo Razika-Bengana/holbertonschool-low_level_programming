0016671009aa11313a9fceebd9f475422555e8be
Razika-Bengana <5003@holbertonschool.com>
2022-07-18 09:25:20 +0200

    more singly linked lists

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -16,9 +16,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	if (index == 0)
+	{
 		*head = (*head)->next;
-	free(list2);
-	return (1);
+		free(list2);
+		return (1);
+	}
 
 	for (i = 0; i < (index - 1); i++)
 	{
