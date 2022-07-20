60b168ba1dbf5b2d8fd660ac2ce6f8d40ea9cd0d
Razika-Bengana <5003@holbertonschool.com>
2022-07-20 12:36:42 +0200

    file I/O

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return (-1);
+		text_content = "";
 
 	while (text_content[a] != '\0')
 	{
