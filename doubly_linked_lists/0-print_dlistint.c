3ef58670b2129580bca8b76e8ddc5ce1f0c74f09
Razika-Bengana <5003@holbertonschool.com>
2022-07-22 10:34:27 +0200

    doubly linked lists

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * print_dlistint - function that prints all the elements of a dlistint_t list
- * @h: a pointer to the double list
+ * @h: a pointer to the doubly linked list
  * Return: the number of nodes
  */
 
