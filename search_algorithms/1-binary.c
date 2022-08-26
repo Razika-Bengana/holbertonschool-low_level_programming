298c74805c35f24511cb1a7d88e75cb88874d977
Razika-Bengana <5003@holbertonschool.com>
2022-08-26 14:35:56 +0200

    search algorithms

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -4,7 +4,8 @@
  * binary_search - function that searches for a value in a sorted array
  * of integers using the Binary search algorithm
  * @array: a pointer to the first element of the array to search in
- * @size: the number of elements in array that will be sorted in ascending order
+ * @size: the number of elements in array that will be sorted
+ * in ascending order
  * @value: the value to search for that won’t appear more than once in array
  * Return: the index where value is located, or -1 if value is not present
  * in array or if array is NULL
